953ad8f234689f13e1af3ff01d49a6e2ba32d7ed
MichelGard <contact@blog-de-michel.fr>
2017-11-04 17:42:04 +0100

    V-0-6-1

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,14 +1,24 @@
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                           <a href="https://www.blog-de-michel.fr" target="_blanck" class="dropdown-toggle">
+                                Faucet CMS V0.6.1 by BlogdeMichel
+                            </a>
+                        </li>
 
-<div class="container-fluid">
-        
-    <div class="row">
-        <div class="col-lg-12"><hr size="4" width="60%" color="black"></div>
+                    </ul>
+                </nav>
+                <p class="copyright pull-right">
+                    &copy; 2016 <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                </p>
+            </div>
+        </footer>
+
     </div>
 </div>
-<div class="container">
-    <div class="row">
-        <?=$footer?>
-        
-    </div>
-    
-</html>
+
+
+</body>
+
---
+++ application/views/templates/menuhaut.php
@@ -0,0 +1,39 @@
+<div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><?=$titreMenu?></a>
+                </div>
+                <div class="collapse navbar-collapse<?=$titreMenu?></a>">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <?=$iconeMenu?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.blog-de-michel.fr" target="_blanck" class="dropdown-toggle" >
+                                Faucet CMS V0.6.1 by BlogdeMichel
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                        <li>
+                           <a href="<?=$param_URL . '/admin/logout'?>">
+                             <i class="fa fa-sign-out" aria-hidden="true"></i>   Log out
+                            </a>
+                        </li>
+                       
+
+                    </ul>
+                </div>
+            </div>
+        </nav>

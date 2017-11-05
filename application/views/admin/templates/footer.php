4b4720c18a316bb794d87bc957d1815771b242e9
MichelGard <contact@blog-de-michel.fr>
2017-11-05 18:19:01 +0100

    V0.6.2.1

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -1,15 +1,15 @@
         <footer class="footer">
             <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
+                <p class="pull-left">
+                    
+                        
                            <a href="https://www.blog-de-michel.fr" target="_blanck" class="dropdown-toggle">
-                                Faucet CMS V0.6.2 by BlogdeMichel
+                                Faucet CMS V0.6.2.1 by BlogdeMichel
                             </a>
-                        </li>
+                        
 
-                    </ul>
-                </nav>
+                    
+                </p>
                 <p class="copyright pull-right">
                     &copy; 2016 <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
                 </p>

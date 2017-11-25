969358075150805f340972e963c8f2063896bdd7
MichelGard <contact@blog-de-michel.fr>
2017-11-25 21:09:09 +0100

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,24 +1,14 @@
-        <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                           <a href="https://www.blog-de-michel.fr" target="_blanck" class="dropdown-toggle">
-                                Faucet CMS V0.6.1 by BlogdeMichel
-                            </a>
-                        </li>
-
-                    </ul>
-                </nav>
-                <p class="copyright pull-right">
-                    &copy; 2016 <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
-                </p>
-            </div>
-        </footer>
-
+    <div class="container-fluid">
+        
+    <div class="row">
+        <div class="col-lg-12"><hr size="4" width="60%" color="black"></div>
     </div>
 </div>
-
-
-</body>
+<div class="container">
+    <div class="row">
+        <?=$footer?>
+        
+    </div>
+    
+</html>
 
